9c15472f5fca550df757c312794e2f10e110e549
kotaro sasaki <sasaki@worksap.co.jp>
2016-02-18 13:00:20 +0900

    DB insert SQL add

--- src/mylocal/disk01/study/main/Sample3.java
+++ src/mylocal/disk01/study/main/Sample3.java
@@ -2,6 +2,7 @@ package mylocal.disk01.study.main;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -17,7 +18,9 @@ public class Sample3 {
 	private static final String DB_USER = "root";
 	private static final String DB_PASS = "maria";
 
-	private static final String SAMPLE_SQL1 = "select id, name from os_mst;";
+	private static final String SAMPLE_SQL1 = "select id, name, verno from os_mst order by id;";
+	//private static final String SAMPLE_INS_SQL1 = "insert into os_mst (id,name,verno,comment) values (10,Ubuntu,14.01,'Ubuntu list');";
+	private static final String SAMPLE_INS_SQL1 = "insert into os_mst (id,name,verno,comment,prc_date) values (?,?,?,?,CURRENT_TIMESTAMP);";
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -90,6 +93,7 @@ public class Sample3 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+//		new Sample3().executeUpd();
 		new Sample3().execute();
 	}
 
@@ -113,12 +117,14 @@ public class Sample3 {
 			System.out.println(
 					rs.getInt(1)
 					+ "," + rs.getString(2)
+					+ "," + rs.getString(3)
 			);
 
 			while (rs.next()) {
 				System.out.println(
 						rs.getInt(1)
 						+ "," + rs.getString(2)
+						+ "," + rs.getString(3)
 				);
 			}
 		} catch (RuntimeException e) {
@@ -133,6 +139,46 @@ public class Sample3 {
 			close(conn);
 		}
 
+	}
+
+	private void executeUpd() {
+		Connection conn = null;
+		PreparedStatement ps = null;
+
+		try {
+			conn = getConnection();
+			ps = conn.prepareStatement(SAMPLE_INS_SQL1);
+
+			ps.setInt(1, 12);
+			ps.setString(2, "Ubuntu14");
+			ps.setString(3, "14.02.01");
+			ps.setString(4, "Ubuntu List");
+
+			ps.executeUpdate();
+			conn.commit();
+
+		} catch (RuntimeException e) {
+			// TODO: handle exception
+		} catch (SQLException e) {
+			// TODO: handle exception
+			logger.error("That Error occurred when access into the DB. ",e);
+
+			try {
+				if (!conn.isClosed()) {
+					conn.rollback();
+				}
+			} catch (SQLException e2) {
+				// TODO: handle exception
+				logger.warn("Failed to rollback.",e2);
+			} catch (UnsupportedOperationException e2) {
+				// TODO: handle exception
+				logger.warn("Failed to rollback.",e2);
+			}
+		} finally {
+			// TODO: handle finally clause
+			close(ps);
+			close(conn);
+		}
 	}
 
 }
